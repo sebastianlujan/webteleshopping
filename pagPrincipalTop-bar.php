1cf12c6c591c5955bfc2f52123e7ce87cffddb77
Diego <diego2737@gmail.com>
2012-10-24 00:11:22 -0200

    modificada extension de readme y agregado topbar de pagina principal

---
+++ pagPrincipalTop-bar.php
@@ -0,0 +1,8 @@
+<ul id="contenedor-topbar">
+	<li id="slidersuperior" ira="index.php" class="topbar-item">Slider superior</li>
+	<li id="slidermedio" ira="PaginaPrincipal-SliderMedio.php" class="topbar-item">Slider medio</li>
+	<li id="videomedio" ira="PaginaPrincipal-VideoMedio.php" class="topbar-item">Video medio</li>
+	<li id="destacado" ira="PaginaPrincipal-ProductoDestacado.php" class="topbar-item">Producto destacado</li>
+	<li id="catalogo" ira="PaginaPrincipal-Catalogo.php" class="topbar-item">Catalogo</li>
+</ul>
+
